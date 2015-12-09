76d7206717c3e70b0ad9de7287bf4a7a6700fd7d
Venj Chu <ersaclarke@gmail.com>
2015-12-09 09:10:51 +0800

    openssl module.

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Sugar",
     dependencies: [
-        .Package(url: "https://github.com/johnno1962/NSLinux.git", versions: "1.0.0" ..< Version.max),
+        .Package(url: "https://github.com/johnno1962/NSLinux.git", versions: Version(1,0,0) ..< Version(2,0,0)),
+        .Package(url: "https://github.com/venj/COpenSSL.git", versions: Version(0,0,1) ..< Version(1,0,0)),
     ]
 )
--- Sources/Sugar/String+MD5.swift
+++ Sources/Sugar/String+MD5.swift
@@ -5,7 +5,6 @@
 //  Created by 朱文杰 on 15/11/3.
 //  Copyright © 2015年 Home. All rights reserved.
 //
-#if !os(Linux)
 
 import Foundation
 #if os(iOS)
@@ -16,6 +15,9 @@ import CommonCrypto_OSX
 import CommonCrypto_tvOS
 #elseif os(watchOS)
 import CommonCrypto_watchOS
+#elseif os(Linux)
+import COpenSSL
+import Glibc
 #endif
 
 // via https://github.com/mnbayan/StringHash
@@ -24,21 +26,21 @@ import CommonCrypto_watchOS
 public extension String {
     /// String's MD5 checksum. 
     var md5: String {
-        let str = self.cStringUsingEncoding(NSUTF8StringEncoding)
-        let strLen = CC_LONG(self.lengthOfBytesUsingEncoding(NSUTF8StringEncoding))
-        let digestLen = Int(CC_MD5_DIGEST_LENGTH)
-        let result = UnsafeMutablePointer<CUnsignedChar>.alloc(digestLen);
-
-        CC_MD5(str!, strLen, result);
-
-        var hash = ""
-        for i in 0 ..< digestLen {
-            hash += String(format:"%02x", arguments:[result[i]])
-        }
-        result.destroy();
-
+        #if os(Linux)
+        let digestLength = Int(MD5_DIGEST_LENGTH)
+        #else
+        let digestLength = CC_MD5_DIGEST_LENGTH
+        #endif
+        var result: [UInt8] = [UInt8(0)] * digestLength
+        let length = strlen(self)
+        #if os(Linux)
+        MD5(self, Int(length), &result)
+        #else
+        CC_MD5(self, UInt32(length), &result)
+        #endif
+        let hash = result.map {
+            String($0, radix: 16, uppercase: true)
+        }.reduce("", combine: +)
         return hash
     }
 }
-
-#endif

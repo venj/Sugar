7cadc251a606455762a0cdc760647d02a146a94f
Venj <ersaclarke@gmail.com>
2015-12-14 15:12:16 +0800

    Reorganized package structure.

--- Swift Package Manager/Package.swift
+++ Swift Package Manager/Package.swift
@@ -6,3 +6,7 @@ let package = Package(
         .Package(url: "https://github.com/venj/COpenSSL.git", versions: Version(0,0,1) ..< Version(1,0,0)),
     ]
 )
+
+#if !os(Linux)
+package.dependencies.append(.Package(url: "https://github.com/venj/CommonCrypto.git", versions: Version(0,0,1) ..< Version(1,0,0)))
+#endif

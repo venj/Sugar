9488a93aa4782558991dde05da2d27539293aa09
Venj <ersaclarke@gmail.com>
2015-12-14 15:26:43 +0800

    Make Common Crypto dependency target more specific.

--- Swift Package Manager/Package.swift
+++ Swift Package Manager/Package.swift
@@ -7,6 +7,6 @@ let package = Package(
     ]
 )
 
-#if !os(Linux)
+#if os(OSX)
 package.dependencies.append(.Package(url: "https://github.com/venj/CommonCrypto.git", versions: Version(0,0,1) ..< Version(1,0,0)))
 #endif
